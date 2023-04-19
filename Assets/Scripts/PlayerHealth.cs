0d039e7f253bb13250474be1dd4f0eeea19a7817
Michael White <michael2white@yahoo.com>
2023-04-19 02:36:26 -0500

    Added black hearts to health bar for damage

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -12,6 +12,10 @@ public class PlayerHealth : MonoBehaviour
     public Image heart2;
     public Image heart3;
 
+    public Image blackHeart1;
+    public Image blackHeart2;
+    public Image blackHeart3;
+
     private void Start()
     {
         currentHealth = maxHealth;   
@@ -20,7 +24,13 @@ public class PlayerHealth : MonoBehaviour
         heart1 = GameObject.Find("GameUI/Heart1").GetComponent<Image>();
         heart2 = GameObject.Find("GameUI/Heart2").GetComponent<Image>();
         heart3 = GameObject.Find("GameUI/Heart3").GetComponent<Image>();
+
+         // Assign the Image components for the black hearts in GameUI
+        blackHeart1 = GameObject.Find("GameUI/BlackHeart1").GetComponent<Image>();
+        blackHeart2 = GameObject.Find("GameUI/BlackHeart2").GetComponent<Image>();
+        blackHeart3 = GameObject.Find("GameUI/BlackHeart3").GetComponent<Image>();
     }
+    
 
     void Update()
     {
@@ -31,21 +41,33 @@ public class PlayerHealth : MonoBehaviour
                 heart1.enabled = true;
                 heart2.enabled = true;
                 heart3.enabled = true;
+                blackHeart1.enabled = false;
+                blackHeart2.enabled = false;
+                blackHeart3.enabled = false;
                 break;
             case 2:
                 heart1.enabled = true;
                 heart2.enabled = true;
                 heart3.enabled = false;
+                blackHeart1.enabled = false;
+                blackHeart2.enabled = false;
+                blackHeart3.enabled = true;
                 break;
             case 1:
                 heart1.enabled = true;
                 heart2.enabled = false;
                 heart3.enabled = false;
+                blackHeart1.enabled = false;
+                blackHeart2.enabled = true;
+                blackHeart3.enabled = true;
                 break;
             case 0:
                 heart1.enabled = false;
                 heart2.enabled = false;
                 heart3.enabled = false;
+                blackHeart1.enabled = true;
+                blackHeart2.enabled = true;
+                blackHeart3.enabled = true;
                 break;
         }
     }
