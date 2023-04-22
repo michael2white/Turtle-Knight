37002aed5e97a98f0ae0dc265800084857f39db7
Michael White <michael2white@yahoo.com>
2023-04-21 22:00:45 -0500

    Added basement to intro level with camera transition

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -11,10 +11,20 @@ public class CameraFollow : MonoBehaviour
     public float dampingTime = 0f;
     private Vector3 velocity = Vector3.zero;
 
+    public float xOffsetThreshold = 135f;
+    public float yPosition = -35f;
+
     // Update is called once per frame
     private void FixedUpdate()
     {
         Vector3 desiredPosition = new Vector3(target.position.x + offset.x, transform.position.y, transform.position.z);
+
+        // Check if the player has moved past the xOffsetThreshold
+        if (target.position.x > xOffsetThreshold)
+        {
+            desiredPosition.y = yPosition;
+        }
+
         Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, dampingTime);
         transform.position = smoothedPosition;
     }
