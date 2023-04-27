d9fe0ee707b4f9516d7f95f1a878de258fbd2c8d
Michael White <michael2white@yahoo.com>
2023-04-27 18:42:03 -0500

    Fixed CameraFollow Script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -13,6 +13,12 @@ public class CameraFollow : MonoBehaviour
 
     public float xOffsetThreshold = 138f;
     public float yPosition = -28.5f;
+    private float originalYPosition;
+
+    private void Start()
+    {
+        originalYPosition = transform.position.y;
+    }
     
 
     // Update is called once per frame
@@ -24,6 +30,11 @@ public class CameraFollow : MonoBehaviour
         if (target.position.x > xOffsetThreshold)
         {
             desiredPosition.y = yPosition;
+        }
+        // Check if player has moved back to left of xOffsetThreshold
+        else if (target.position.x < xOffsetThreshold)
+        {
+            desiredPosition.y = originalYPosition;
         }
 
         Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, dampingTime);
